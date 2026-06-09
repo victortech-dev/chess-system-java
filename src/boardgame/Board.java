abb12d62f735f5503435cd897d43fd05392c8789
vkdev <dev.victorkaua@gmail.com>
2026-06-08 21:43:05 -0300

    complements BoardExecption

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -25,26 +25,38 @@ public class Board {
         return columns;
     }
     public Piece piece(int row, int column){
+        if(!positionExists(row,column)){
+            throw new BoardException("Position not on the board");
+
+        }
         return pieces[row][column];
         
     }
     public Piece piece(Position position){
+
         return pieces[position.getRow()][position.getColumn()];
     }
     public void placePiece(Piece piece, Position position){
+        if(thereIsApiece(position)){
+            throw new BoardException("There is already a piece on" + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
 
-    private boolean positionExsists(int row, int column){
+    private boolean positionExists(int row, int column){
         return row >= 0 && row < rows && column >= 0 && column < columns;
     }
 
-    public boolean postionExists(Position position){
-        return positionExsists(position.getRow(), position.getColumn());
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
     }
 
     public boolean thereIsApiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position not on the board");
+
+        }
         return piece(position) != null;
     }
 
